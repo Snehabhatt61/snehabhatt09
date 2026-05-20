a4cfa5d428594d4347c17c2e8ae3142a50d8e95a
snehabhatt61 <snehabhatt61@gmail.com>
2026-05-20 20:03:31 +0530

    [FIX] : Fixed bugs

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -2,13 +2,41 @@ const load = (id, url) => {
   fetch(url)
     .then(response => response.text())
     .then(html => {
-      document.getElementById(id).innerHTML = html;
-    });
-};
+      document.getElementById(id).innerHTML = html
 
-// Load components for index.html
-load('header-placeholder', '../components/header.html');
-load('footer-placeholder', '../components/footer.html');
+      // Fix relative links in loaded components
+      const isInPagesFolder = window.location.pathname.includes('/pages/')
+      const container = document.getElementById(id)
 
-// Load components for projects.html
-// (This will be handled in projects.html separately)
+      if (isInPagesFolder) {
+        // We're in pages folder, links should go up one level
+        container.querySelectorAll('a').forEach(link => {
+          const href = link.getAttribute('href')
+          if (href && !href.startsWith('http') && !href.startsWith('../')) {
+            // Prepend ../ to relative links that don't already have it
+            if (href.startsWith('./')) {
+              link.setAttribute('href', '../' + href.substring(2))
+            }
+          }
+        })
+
+        container.querySelectorAll('img').forEach(img => {
+          const src = img.getAttribute('src')
+          if (src && !src.startsWith('http') && !src.startsWith('../')) {
+            if (src.startsWith('./')) {
+              img.setAttribute('src', '../' + src.substring(2))
+            }
+          }
+        })
+      }
+    })
+    .catch(error => console.error(`Failed to load ${url}:`, error))
+}
+
+// Detect if we're in pages folder or root
+const isInPagesFolder = window.location.pathname.includes('/pages/')
+const basePath = isInPagesFolder ? '../' : './'
+
+// Load components with correct relative paths
+load('header-placeholder', basePath + 'components/header.html')
+load('footer-placeholder', basePath + 'components/footer.html')
